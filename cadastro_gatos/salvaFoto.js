d9c21758cfec884ddbcf8066b81df6dfbf64a14c
MATHEUS FRANCISCO FERREIRA DOS SANTOS <matheus111111111111@gmail.com>
2023-05-08 00:15:06 -0300

    teste
    testando o sistema de salvamento de cache, 95% concluido

--- cadastro_gatos/salvaFoto.js
+++ cadastro_gatos/salvaFoto.js
@@ -1,16 +1,25 @@
-const inputFile = document.getElementById('input-file');
-const previewImage = document.getElementById('preview-image');
+document.addEventListener('DOMContentLoaded', function() {
+  const inputFile = document.getElementById('input-file');
+  const previewImage = document.getElementById('preview-image');
 
-inputFile.addEventListener('change', function() {
-  const file = this.files[0];
-  
-  if (file) {
-    const reader = new FileReader();
-    
-    reader.addEventListener('load', function() {
-      previewImage.src = reader.result;
-    });
-    
-    reader.readAsDataURL(file);
+  inputFile.addEventListener('change', function() {
+    const file = this.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.addEventListener('load', function() {
+        previewImage.src = reader.result;
+        localStorage.setItem('previewImage', reader.result);
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+
+  const cachedImage = localStorage.getItem('previewImage');
+
+  if (cachedImage) {
+    previewImage.src = cachedImage;
   }
 });
